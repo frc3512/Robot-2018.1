Team3512/Robot-2018

Added all switch based and autoline auton modes
Change-Id: Ic4a86d3ffb601274be040f04d45189aeb30c411a

src/AutonomousModes/AutoAutoLine.cpp
@@ -1,0 +1,32 @@
+// Copyright (c) 2016-2018 FRC Team 3512. All Rights Reserved.
+
+#include "Robot.hpp"
+
+enum class State { Init, MoveForward, Idle };
+
+// Drives forward until passing white line 120 inches away from start
+void Robot::AutoAutoLine() {
+    static State state = State::Init;
+
+    switch (state) {
+        case State::Init:
+            robotDrive.StartClosedLoop();
+
+            robotDrive.ResetEncoders();
+            robotDrive.ResetGyro();
+            robotDrive.SetPositionReference(k_robotLength + 120);  // Estimate
+            robotDrive.SetAngleReference(0);
+
+            state = State::MoveForward;
+            break;
+        case State::MoveForward:
+            if (robotDrive.PosAtReference()) {
+                robotDrive.StopClosedLoop();
+
+                state = State::Idle;
+            }
+            break;
+        case State::Idle:
+            break;
+    }
+}

frc3512/Robot-2018.1

src/AutonomousModes/AutoAutoLine.cpp
@@ -1,0 +1,32 @@
+// Copyright (c) 2016-2018 FRC Team 3512. All Rights Reserved.
+
+#include "Robot.hpp"
+
+enum class State { kInit, kMoveForward, kIdle };
+
+// Drives forward until passing white line 120 inches away from start
+void Robot::AutoAutoLine() {
+    static State state = State::kInit;
+
+    switch (state) {
+        case State::kInit:
+            robotDrive.StartClosedLoop();
+
+            robotDrive.ResetEncoders();
+            robotDrive.ResetGyro();
+            robotDrive.SetPositionReference(k_robotLength + 120);  // Estimate
+            robotDrive.SetAngleReference(0);
+
+            state = State::kMoveForward;
+            break;
+        case State::kMoveForward:
+            if (robotDrive.PosAtReference()) {
+                robotDrive.StopClosedLoop();
+
+                state = State::kIdle;
+            }
+            break;
+        case State::kIdle:
+            break;
+    }
+}