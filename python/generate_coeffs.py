1f8a87cd2e311126238cde1d62c7a065b469752d
Tyler Veness <calcmogul@gmail.com>
2019-02-02 10:24:23 -0800

    Add format.py for formatting Python files using black
    Change-Id: Icb5fe033fe7e62f037ff723f74a8b1f722046441

--- python/generate_coeffs.py
+++ python/generate_coeffs.py
@@ -5,11 +5,8 @@ import subprocess
 import sys
 
 subprocess.run([sys.executable, "drivetrain.py", "--noninteractive"])
-os.rename("DrivetrainCoeffs.hpp",
-          "../src/main/include/control/DrivetrainCoeffs.hpp")
-os.rename("DrivetrainCoeffs.cpp",
-          "../src/main/cpp/control/DrivetrainCoeffs.cpp")
+os.rename("DrivetrainCoeffs.hpp", "../src/main/include/control/DrivetrainCoeffs.hpp")
+os.rename("DrivetrainCoeffs.cpp", "../src/main/cpp/control/DrivetrainCoeffs.cpp")
 subprocess.run([sys.executable, "elevator.py", "--noninteractive"])
-os.rename("ElevatorCoeffs.hpp",
-          "../src/main/include/control/ElevatorCoeffs.hpp")
+os.rename("ElevatorCoeffs.hpp", "../src/main/include/control/ElevatorCoeffs.hpp")
 os.rename("ElevatorCoeffs.cpp", "../src/main/cpp/control/ElevatorCoeffs.cpp")
